f19e49139f7a89e68233dff9c7f975d76462b235
Colab X <145373938+ColabX2001@users.noreply.github.com>
2023-10-01 05:03:23 +0700

    update ts-loader to babel-loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { webpack } = require('webpack');
+const webpack = require('webpack');
 const buildPath = path.join(__dirname, '../build');
+const TerserPlugin = require("terser-webpack-plugin");
 
 const isDev = process.env.NODE_ENV == "development"
 const assetsPath = 'assets'
@@ -10,25 +11,49 @@ const assetsPath = 'assets'
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    filename: isDev ? assetsPath+'/js/bundle.js' : assetsPath+'/js/[name].[hash].js',
+    filename: isDev ? assetsPath + '/js/[name].bundle.js' : assetsPath + '/js/[name].[hash].js',
     path: buildPath,
     publicPath: "/",
     chunkFilename: '[name].chunk.[chunkhash].js'
   },
+  experiments: {
+    topLevelAwait: true,
+  },
   optimization: {
+    splitChunks: {
+      cacheGroups: {
+        appVendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+    // runtimeChunk: {
+    //   name: (entrypoint) => `runtime~${entrypoint.name}`,
+    // },
     minimize: !isDev,
+    minimizer: [new TerserPlugin()],
   },
   mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: 'ts-loader',
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.s[ac]ss$/,
@@ -45,13 +70,13 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg|gif)$/i,
+        test: /\.(png|jpg|jpeg|gif)$/i,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 8192,
-              name: assetsPath+'/images/[name].[hash].[ext]',
+              name: assetsPath + '/images/[name].[hash].[ext]',
               publicPath: './'
             },
           },
@@ -64,16 +89,27 @@ module.exports = {
             loader: 'svg-url-loader',
             options: {
               limit: 10000,
-              name: assetsPath+'/images/[name].[hash].[ext]',
+              name: assetsPath + '/images/[name].[hash].[ext]',
             },
           },
         ],
+      },
+      {
+        test: /\.ttf$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: assetsPath + '/fonts/[name].[hash].[ext]',
+            emitFile: false,
+          },
+        },
       },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  devtool: false,
   plugins: [
     new HtmlWebpackPlugin({
       template: './static/index.html',
@@ -83,12 +119,19 @@ module.exports = {
       favicon: "./static/favicon.ico"
     }),
     new MiniCssExtractPlugin({
-        filename: assetsPath+'/css/[name].[hash].css',
+      filename: assetsPath + '/css/[name].[hash].css',
 
+    }),
+    new webpack.DefinePlugin({
+      __REACT_DEVTOOLS_GLOBAL_HOOK__: `({ isDisabled: true })`,
     }),
   ],
   devServer: {
     historyApiFallback: { index: "/", disableDotRule: true },
-    open: true
-  }
+    open: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
+  },
+  
 };
